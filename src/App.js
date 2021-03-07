00bc7284d33f4ef2d19fdc2a209db9513e0ddb24
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-03-08 01:04:21 +0600

    React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import {Button, Jumbotron} from 'react-bootstrap';
 import News from './components/News/News';
 import Header from './components/Header/Header';
 import TopHeadline from './components/TopHeadline/TopHeadline';
+import { Fragment } from 'react';
 
 
 function App() {
   return (
     <div>
       <h1>React Bootstrap</h1>
+      <Fragment></Fragment>
       <Header></Header>
       <TopHeadline></TopHeadline>
       
---
+++ src/components/Fragment/Fragment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Fragment = () => {
+    return (
+        <>
+        <p>Fragment -1 </p>
+        <p>Fragment -2 </p>
+        </>
+    );
+};
+
+export default Fragment;
